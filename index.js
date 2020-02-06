a08bc0824ce253a668ae2db8c10551d6ad044b68
Kathleen Kamali <kathleenkamali@gmail.com>
2020-02-06 11:10:11 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(source, f) {
+  const newArray = []
+  for (let i = 0; i < source.length; i++) {
+    newArray.push(f(source[i]))
+  }
+  return newArray
+}
+
+function reduce(source, f, starting) {
+  let reduced = (!!starting) ? starting : source[0]
+  let i = (!!starting) ? 0 : 1
+  for (i; i < source.length; i++) {
+    reduced = f(source[i], reduced)
+  }
+  return reduced
+}
